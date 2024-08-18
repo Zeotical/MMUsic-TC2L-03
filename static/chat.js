acd131658b4bdaa3285497f7fdb962b6c145c4a4
peishan132 <1231103069@student.mmu.edu.my>
2024-08-19 00:19:28 +0800

    Everything

---
+++ static/chat.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    var socket = io.connect('http://' + document.domain + ':' + location.port);
+
+    socket.on('connect', function() {
+        socket.emit('joined', {});
+    });
+
+    socket.on('message', function(data) {
+        $('#messages').append('<li>' + data.username + ': ' + data.text + '</li>');
+    });
+
+    $('#send').click(function() {
+        var message = $('#txt').val();
+        socket.emit('text', {text: message});
+        $('#txt').val('');
+    });
+
+    $('#txt').on('keypress', function(e) {
+        if(e.which === 13) {
+            $('#send').click();
+        }
+    });
+});
