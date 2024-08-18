472d4a3a4813c4a66ba5b82029a84517030c63ee
peishan132 <1231103069@student.mmu.edu.my>
2024-08-19 00:19:28 +0800

    Everything

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("hello,world")
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+socketio = SocketIO(app)
+
+@app.route('/')
+def chat():
+    return render_template('chat.html')
+
+@socketio.on('joined')
+def handle_joined(data):
+    emit('message', {'username': 'System', 'text': 'Welcome to Music Chat'}, broadcast=True)
+    
+@socketio.on('text')
+def handle_text(data):
+    text = data['text']
+    username = 'User'
+    emit('message', {'username': username, 'text': text}, broadcast=True)
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
