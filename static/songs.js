7d0fef02648138f12e82390a7d743cd004758f39
ZIyi05 <ziyimow@gmail.com>
2024-09-21 17:39:32 +0800

    icon

--- static/songs.js
+++ static/songs.js
@@ -29,20 +29,51 @@ $(document).ready(function() {
         }
     });
 
+    // Hide the search result when clicking outside
     $(document).on('click', function(e) {
         if (!$(e.target).closest('#text').length && !$(e.target).closest('#show-list').length) {
             $('#show-list').hide();
         }
     });
 
+    // Handle click on search result
     $(document).on('click', '.link-class', function() {
         var selectedFile = $(this).data('file');
         var filePath = '/static/music.mp3/' + selectedFile;
         console.log("Selected file path: ", filePath);
+
+        // Set audio player source and play the selected song
         $('#audio_player').attr('src', filePath);
+        var audioPlayer = document.getElementById('audio_player');
+        audioPlayer.play().then(() => {
+            console.log('Playing the song');
+        }).catch((error) => {
+            console.error('Error playing the song:', error);
+        });
+
+        // Get the lyrics from the selected song
         var lyrics = $(this).find('small').text();
-        $('#messages').append(`<li>User: ${lyrics}</li>`);
+
+        // Append the profile picture, username, lyrics, and play icon to the chat
+        $('#messages').append(`
+            <li class="chat-message">
+                <img src="${pfp_url}" class="chatpfp"> ${username}: ${lyrics} 
+                <ion-icon name="play-circle-outline" class="play-icon" style="cursor:pointer;" data-file="${selectedFile}"></ion-icon>
+            </li>
+        `);
+
+        // Clear the search box after selecting the lyric
+        $('#text').val('');
         $('#show-list').hide();
+    });
+
+    // Play song when play icon is clicked
+    $(document).on('click', '.play-icon', function() {
+        var selectedFile = $(this).data('file');
+        var filePath = '/static/music.mp3/' + selectedFile;
+
+        // Update the audio player source and play the selected song
+        $('#audio_player').attr('src', filePath);
         var audioPlayer = document.getElementById('audio_player');
         audioPlayer.play().then(() => {
             console.log('Playing the song');
@@ -51,4 +82,3 @@ $(document).ready(function() {
         });
     });
 });
-
